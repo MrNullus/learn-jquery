326ded89566458c543358f1d646a48707f10ed7d
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-15 16:32:29 -0300

    mod: finally lecture

--- basic/mod/01/js/aula07.js
+++ basic/mod/01/js/aula07.js
@@ -2,9 +2,48 @@ $(function () {
 
 	let ex = $('.ex1');
 
-	$('place').each(function () {
-        let input $(this);
+	$('.place').each(function () {
+
+  	let input = $(this);
 		let place = $(this).attr("title");
+
+		input
+			.val(place)
+			.css("color", "#ccc")
+			.focusin(function () {
+				input.css("color", "#000");
+
+				if (input.val() == place) {
+					input.val('');
+				}
+			})
+			.focusout(function () {
+				if (input.val() == '') {
+					input.css("color", "#ccc");
+					input.val(place);
+				}
+			});
+
+	});
+
+
+	let ex = $('.ex1');
+	// # Primeiro exemplo
+	// $('.key').keypress(function () {
+	// 	let text = $(this).val();
+	// 	ex.text(text);
+	// });
+
+	// # Segundo exemplo
+	$('.key').keydown(function () {
+		let text = $(this).val();
+		ex.text(text);
+	});
+
+	// # Terceiro exemplo
+	$('.key').keyup(function () {
+		let text = $(this).val();
+		ex.text(text);
 	});
 
 });
