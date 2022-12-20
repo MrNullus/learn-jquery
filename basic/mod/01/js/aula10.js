c49cc48dd013fc02f71b44ae3dc5a16bc5607323
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-19 21:58:48 -0300

    add fikes

---
+++ basic/mod/01/js/aula10.js
@@ -0,0 +1,23 @@
+$(function () {
+
+	let ex  = $('.ex1');
+  let e2  = $('.ex2');
+  let btn = $('button[type=button]');
+
+  btn
+    .click(function () {
+      ex.fadeOut('slow');
+    })
+    .dbclick(function () {
+      ex.fadeIn('slow');
+    });
+
+  btn.mouseover(function () {
+    // -> o segundo parametro é a opacidade que o elemento receberá
+    // -> o terceiro é um callback
+    ex.fadeTo(3000, 0.8, function () {
+      ex2.fadeTo('show', 0.2);
+    });
+  })
+
+});
